f1e66c24e18609fd0be028b9aa73c0a578ade6e8
aikikaii <53414211+aikikaii@users.noreply.github.com>
2019-10-24 18:19:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //creating global variables
 
 const studentList = document.querySelector('.student-list');
-const li = document.querySelector('.student-item');
+const li = document.querySelectorAll('.student-item');
+
+//checking what variable li selects
+console.log(li);
 
 //creating function showPage 
 
@@ -9,16 +12,15 @@ function showPage(list, page) {
     let startIndex = (page * 10) - 10;
     let endIndex = page * 10;
     for (let i = 0; i < list.length; i++) {
-        if (i >= startIndex && i < endIndex) {
-            list.style.display = 'list-item';
+        if (i >= startIndex[list] && i < endIndex[list]) {
+            list.style.display = "list-item";
         } else {
             list.style.display = 'none';
         }
     }
 
 }
-showPage(studentList, 2);
-
+showPage(li, 1);
 
 
 
@@ -27,6 +29,17 @@ showPage(studentList, 2);
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(list) {
+    const div = document.querySelector('.pagination');
+    let ul = ul.document.createElement('ul');
+    let li = li.document.createElement('li');
+
+
+    ul = li.parentNode;
+    ul.appendChild(li);
+    div = ul.parentNode;
+    div.appendChild(ul);
+}
 
 
 
